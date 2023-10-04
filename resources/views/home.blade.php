2401007ab27649a20e9cd9f314c4f340d1497309
Muhammad-Zikri23 <muhammadzikri237@gmail.com>
2023-10-04 17:48:12 +0700

    Tugas Zikri

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet"
+href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.mi
+n.css">
+    <title>Homepage</title>
+</head>
+<body>
+    <h1 class="position-absolute top-50 start-50 translate-
+middle">SAYA RISWANDA DARI KELAS RPL5B 
+BERJANJI  UNTUK BISA MEMBUAT 
+APLIKASI WEB SENDIRI!</h1>
+    <script
+src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bund
+le.min.js"></script>
+</body>
+</html>
